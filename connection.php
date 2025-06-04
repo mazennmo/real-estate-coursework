67940c4307eccf58b96d156dc4ee3e758d6a0a85
mazennmo <elbeltagy.m@oundleschool.org.uk>
2025-06-04 13:03:52 +0100

    connection

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username="root";
 $password="";
-$dbname="real-estate-coursework";
+$dbname="realestate";
 
 $conn =new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
